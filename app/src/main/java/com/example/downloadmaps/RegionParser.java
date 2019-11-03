6280414c1ab4b6a6ec4e1929258ff99daa04b06f
Dima-1 <softmail32@gmail.com>
2019-11-03 17:51:08 +0300

    Parsing with link to sub regions

--- app/src/main/java/com/example/downloadmaps/RegionParser.java
+++ app/src/main/java/com/example/downloadmaps/RegionParser.java
@@ -65,24 +65,24 @@ class RegionParser {
 			if (parser.getEventType() != XmlPullParser.START_TAG) {
 				continue;
 			}
-			String prefix = "";
 			String name = parser.getName();
 			if (name.equals("region")) {
-
 				String mapFileName = "";
-				if (parser.getAttributeValue(null, "map") == null
-						|| (parser.getAttributeValue(null, "map") != null && !parser.getAttributeValue(null, "map").equals("no")))
+				if (parser.getAttributeValue(null, "map") == null || !isAttrMapEqualsNo(parser)) {
 					mapFileName = parser.getAttributeValue(null, "name");
-				Log.d(TAG, "readRegion: " + (parser.getAttributeValue(null, "name")
-						+ " - " + mapFileName));
-				entries.add(new Entry((parser.getAttributeValue(null, "name")), mapFileName, null));
-				entries.addAll(readSubRegion(parser, parser.getAttributeValue(null, "name")));
+				}
+				Log.d(TAG, "readRegion: " + (parser.getAttributeValue(null, "name") + " - " + mapFileName));
+				Entry region = new Entry((parser.getAttributeValue(null, "name")), mapFileName, null);
+				entries.add(region);
+				entries.addAll(readSubRegion(parser, parser.getAttributeValue(null, "name"), region));
 			}
 		}
 		return entries;
 	}
 
-	private ArrayList<Entry> readSubRegion(XmlPullParser parser, String prefix) throws IOException, XmlPullParserException {
+	private ArrayList<Entry> readSubRegion(XmlPullParser parser, String prefix, Entry parent)
+			throws IOException, XmlPullParserException {
+
 		ArrayList<Entry> entries = new ArrayList<>();
 		parser.require(XmlPullParser.START_TAG, ns, "region");
 		while (parser.next() != XmlPullParser.END_TAG) {
@@ -92,17 +92,28 @@ class RegionParser {
 			String name = parser.getName();
 			if (name.equals("region")) {
 				String mapFileName = "";
-				if (parser.getAttributeValue(null, "map") == null
-						|| (parser.getAttributeValue(null, "map") != null && !parser.getAttributeValue(null, "map").equals("no")))
+				if (parser.getAttributeValue(null, "map") == null || !isAttrMapEqualsNo(parser)) {
 					mapFileName = prefix + "_" + parser.getAttributeValue(null, "name");
-				Log.d(TAG, "readSubRegion: " + parser.getLineNumber() + " " + parser.getAttributeValue(null, "name")
-						+ " -- " + mapFileName);
-				if (!(parser.getAttributeValue(null, "type") != null && !parser.getAttributeValue(null, "type").equals("map")))
-					entries.add(new Entry(parser.getAttributeValue(null, "name"), mapFileName, null));
-				entries.addAll(readSubRegion(parser, prefix + "_" + parser.getAttributeValue(null, "name")));
+				}
+				Log.d(TAG, "readSubRegion: " + parser.getLineNumber()
+						+ " " + parser.getAttributeValue(null, "name")
+						+ " = " + mapFileName + " __^"
+						+ ((parent != null) ? parent.getName() : "--"));
+				Entry subRegion = new Entry(parser.getAttributeValue(null, "name"), mapFileName, parent);
+				if (!(parser.getAttributeValue(null, "type") != null
+						&& !parser.getAttributeValue(null, "type").equals("map"))) {
+					entries.add(subRegion);
+				}
+				entries.addAll(readSubRegion(parser,
+						prefix + "_" + parser.getAttributeValue(null, "name"), subRegion));
 			}
 		}
 		return entries;
+	}
+
+	private boolean isAttrMapEqualsNo(XmlPullParser parser) {
+		return parser.getAttributeValue(null, "map") != null
+				&& parser.getAttributeValue(null, "map").equals("no");
 	}
 
 	private String createMapFileName(String name) {
